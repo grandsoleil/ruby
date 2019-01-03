4e53a47f1835a5e25e33e7f5abead688fa34e9d0
raphaelcinelli <cinelliraphael@gmail.com>
2019-01-03 19:02:51 +0100

    chez moi cela n'empeche pas l'execution du programme

--- exo_03.rb
+++ exo_03.rb
@@ -1,1 +1,1 @@
-puts "ruby exo_02.rb\n#{}Bonjour, monde !\nEt avec une voix sexy, ça donne : Bonjour, monde !"
+puts "ruby exo_03.rb\n#{}Bonjour, monde !\nEt avec une voix sexy, ça donne : Bonjour, monde !"
