615dc2bbcfd9493f41579984a4712915d29ea4a1
raphaelcinelli <cinelliraphael@gmail.com>
2019-01-05 00:44:12 +0100

    1ere pyramide

--- exo_20.rb
+++ exo_20.rb
@@ -1,3 +1,11 @@
-puts "Donne moi un nombre ?"
-nombre=gets
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+number=gets
 
+puts "Voici la pyramide :"
+
+dieze = "#"
+etage = number.to_i - number.to_i + 1
+while etage <= number.to_i
+	puts dieze * etage
+	etage += 1
+end
